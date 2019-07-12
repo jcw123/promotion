03c51c4db7248a662fc147fd7424902da2fb828c
jiangcw <1433179149@qq.com>
2019-07-12 08:47:19 +0800

    数据结构学习

---
+++ src/main/java/com/im/sky/datastructure/map/ArrayRepresentationGraphBuild.java
@@ -0,0 +1,18 @@
+package com.im.sky.datastructure.map;
+
+/**
+ * Created by jiangchangwei on 2019/7/11.
+ *
+ * 建立数组表示法的图结构
+ *
+ * （1）一个数组或者hash结构用于存储顶点；
+ * （2）一个二维数组用于存储顶点之间的关系
+ */
+public class ArrayRepresentationGraphBuild implements GraphBuild {
+
+
+    @Override
+    public void build() {
+
+    }
+}
---
+++ src/main/java/com/im/sky/datastructure/map/GraphBuild.java
@@ -0,0 +1,9 @@
+package com.im.sky.datastructure.map;
+
+/**
+ * Created by jiangchangwei on 2019/7/11.
+ */
+public interface GraphBuild {
+
+    void build();
+}
